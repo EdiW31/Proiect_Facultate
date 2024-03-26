39c3595abf26ad636e2885b2c2dc0de0a16baa19
EdiW31 <129823940+EdiW31@users.noreply.github.com>
2024-03-26 08:54:21 +0200

    Update loginpage

--- src/Components/LoginPage/loginpage.component.jsx
+++ src/Components/LoginPage/loginpage.component.jsx
@@ -89,13 +89,6 @@ function LoginPage() {
     <div className="min-h-screen bg-gray-100 text-gray-900 flex justify-center">
       <div className="max-w-screen-xl    m-0 sm:m-10 bg-white shadow sm:rounded-lg flex justify-center flex-1">
         <div className="lg:w-1/2 xl:w-5/12 p-6 sm:p-12">
-          <div>
-            <img
-              src="https://storage.googleapis.com/devitary-image-host.appspot.com/15846435184459982716-LogoMakr_7POjrN.png"
-              className="w-32 mx-auto"
-              alt="Logo"
-            />
-          </div>
           <div className="mt-12 flex flex-col items-center">
             <h1 className="text-2xl xl:text-3xl font-extrabold">
               {loginType === "login"
