724c5976ca5a6fd1bca4704f1b9d2ae29f89b4bb
EdiW31 <129823940+EdiW31@users.noreply.github.com>
2024-05-30 12:17:21 +0300

    final commit #5

--- src/Components/LoginPage/loginpage.component.jsx
+++ src/Components/LoginPage/loginpage.component.jsx
@@ -205,6 +205,15 @@ function LoginPage() {
                 >
                   Reset Password
                 </p>
+                <p>
+                  <a href="www.csac.ro">CSAC</a>
+                </p>
+                <p>
+                  <a href="https://scanstart.ro/">ScanStat</a>
+                </p>
+                <p>
+                  <a href=" https://graffino.com/">Graffino</a>
+                </p>
               </div>
             </div>
           </div>
